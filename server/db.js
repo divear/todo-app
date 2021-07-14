deb63600b03028c708fcb05b5fe9a1120596d9b6
Lukas <lukyn.odehnal333@gamil.com>
2021-07-14 15:24:38 +0200

    db connection

--- server/db.js
+++ server/db.js
@@ -1,8 +1,9 @@
 const Pool = require("pg").Pool;
+require("dotenv").config()
 
 const pool = new Pool({
     user: process.env.NAME,
-    password: process.env.password,
+    password: process.env.PASSWORD,
     host: "localhost",
     port: 5432,
     database: "todo"
--- server/index.js
+++ server/index.js
@@ -3,9 +3,11 @@ const app = express()
 const cors = require("cors")
 const pool = require("./db")
 
+
 //middleware
 app.use(cors());
 app.use(express.json())
+
 
 //ROUTES//
 
@@ -13,7 +15,10 @@ app.use(express.json())
 
 app.post("/todos", async(req, res)=>{
     try {
-        console.log(req.body);
+        const respo = req.body
+        console.log(respo);
+        const newTodo = await pool.query("INSERT INTO todo (body, username) VALUES($1, $2) RETURNING *", [respo.body, respo.username]);
+        res.json(newTodo.rows[0])
     } catch (error) {
         console.log(error);
     }
@@ -21,11 +26,52 @@ app.post("/todos", async(req, res)=>{
 
 //get all
 
+app.get("/todos", async (req, res) => {
+    try {
+      const allTodos = await pool.query("SELECT * FROM todo");
+      res.json(allTodos.rows);
+    } catch (err) {
+      console.error(err.message);
+    }
+  });
+  
+
 //get one
+
+app.get("/todos/:id", async(req, res)=>{
+    try {
+        const {id} = req.params
+        const todo = await pool.query("SELECT * FROM todo WHERE id = $1", [id])
+        
+        res.json(todo.rows[0])
+    } catch (error) {
+        console.log(error);
+    }
+})
 
 //update 
 
+app.put("/todos/:id", async(req, res)=>{
+    try {
+        const {id} = req.params
+        const {body} = req.body
+        const updateTodo = await pool.query("UPDATE todo SET body = $1 WHERE id = $2", [body, id]);
+        res.json("todo was updated")
+    } catch (error) {
+        console.log(error);
+    }
+})
+
 //delete
+
+app.delete("/todos/:id", async(req, res)=>{
+    try {
+        const {id} = req.params
+        const deleteTodo = await pool.query("DELETE FROM todos WHERE id = $1",[id])
+    } catch (error) {
+        console.log(error);
+    }
+})
 
 const port = process.env.port || 4000
 app.listen(port, ()=>{
